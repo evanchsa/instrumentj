1e7ce21b27d0576b3658c679fd0dd0b3e2e8ae1f
Stephen Evanchik <evanchsa@gmail.com>
2014-07-14 23:03:55 -0400

    examples: Example StatefulMethodProbe
     - Add an example StatefulMethodProbe

--- examples/instrumentj.test/src/main/java/test/instrumentj/Main.java
+++ examples/instrumentj.test/src/main/java/test/instrumentj/Main.java
@@ -1,19 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package test.instrumentj;
 
+import instrumentj.Exclude;
 import instrumentj.StaticProfilerInterface;
+import instrumentj.probes.Probe;
 import instrumentj.probes.impl.LoggingMethodEntryProbe;
 import instrumentj.probes.impl.LoggingMethodExitProbe;
+import instrumentj.probes.impl.StatefulMethodProbe;
 import test.instrumentj.impl.SomeClass;
 
+/**
+ * @author Stephen Evanchik (evanchsa@gmail.com)
+ *
+ */
 public class Main implements Runnable {
 
     private final SomeClass someClass = new SomeClass();
+
+    @Exclude
+    static class TestStatefulMethodProbe extends StatefulMethodProbe {
+
+		@Override
+		public String getClassNameFilter() {
+			return Main.class.getName();
+		}
+
+		@Override
+		public String getMethodNameFilter() {
+			return Probe.WILDCARD;
+		}
+
+		@Override
+		public String getMethodDescriptionFilter() {
+			return Probe.WILDCARD;
+		}
+
+		@Override
+		protected void doRun(boolean exiting, Object... args) {
+
+			if (exiting) {
+				System.out.println("Stateful exiting method: " + args[0] + "|" + args[1] + "|" + args[2]);
+			} else {
+				System.out.println("Stateful entering method: " + args[0] + "|" + args[1] + "|" + args[2]);
+			}
+		}
+	}
+
     /**
      * @param args
      */
     public static void main(String[] args) {
         StaticProfilerInterface.INSTANCE.getProfiler().addProbe(new LoggingMethodEntryProbe());
         StaticProfilerInterface.INSTANCE.getProfiler().addProbe(new LoggingMethodExitProbe());
+        StaticProfilerInterface.INSTANCE.getProfiler().addProbe(new TestStatefulMethodProbe());
 
         final Main m = new Main();
         m.run();
@@ -28,7 +81,7 @@ public class Main implements Runnable {
 
                 Thread.sleep(10 * 1000);
             }
-        } catch (Throwable t) {
+        } catch (final Throwable t) {
 
         }
     }
--- examples/instrumentj.test/src/main/java/test/instrumentj/impl/SomeClass.java
+++ examples/instrumentj.test/src/main/java/test/instrumentj/impl/SomeClass.java
@@ -1,13 +1,37 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 package test.instrumentj.impl;
 
+/**
+ * @author Stephen Evanchik (evanchsa@gmail.com)
+ *
+ */
 public class SomeClass {
 
     public void aMethod() {
         System.out.println("aMethod");
+
+        cMethod();
     }
 
     public void bMethod() {
         System.out.println("bMethod");
     }
 
+    public void cMethod() {
+        System.out.println("cMethod");
+    }
 }
